d45912b3a89dbf33dfe81597b5e24fd35eaf21e6
antimonysayz <antimonysayz@gmail.com>
2020-12-17 18:58:28 -0500

    testing bcrypt and user creation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,23 @@
 class UsersController < ApplicationController
+
+
+    def create
+        User.create(user_params) 
+    end
+
+    def index
+        users = User.all
+        render json: users
+    end
+
+    # def show 
+    #     user = User.find_by(id: params[:id])
+    #     render json: user
+    # end
+
+    def user_params
+        params.permit(:username, :password)
+    end
+
+
 end
