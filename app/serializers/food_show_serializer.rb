6c45e337b2072d1a7f02a669457bc4b680dd2124
antimonysayz <antimonysayz@gmail.com>
2020-12-27 22:26:46 -0500

    adding nutrient_hash to food seralizer

--- app/serializers/food_show_serializer.rb
+++ app/serializers/food_show_serializer.rb
@@ -1,7 +1,8 @@
 class FoodShowSerializer < ActiveModel::Serializer
-    attributes :id, :name, :kj, :calories, :total_lipid, :protein, :water, :carbs, :fiber, :poly_unsat_fats, 
+    attributes :id, :name, :nutrient_hash, :kj, :calories, :total_lipid, :protein, :water, :carbs, :fiber, :poly_unsat_fats, 
     :sat_fats, :cholesterol, :vitamin_b12, :folate_dfe, :food_folate, :folic_acid, :total_folate, :vitamin_b6, 
     :pantothenic, :niacin, :riboflavin, :thiamin, :vitamin_c, :vitamin_e, :vitamin_d, :vitamin_a_r, :vitamin_a,
     :selenium, :copper, :zinc, :sodium, :magnesium, :iron, :calcium, :ash, :galactose, :lactose, :fructose, :glucose,
     :sucrose, :total_sugar, :starch
+    
 end
