359320b43caa1fc9f8c86d6f17238760c4e45657
anthonym5 <43191869+AnthonyM5@users.noreply.github.com>
2021-02-27 18:11:51 -0500

    adding validation for food model

--- app/models/food.rb
+++ app/models/food.rb
@@ -1,6 +1,7 @@
 class Food < ApplicationRecord
     has_many :ingredients
     has_many :meals, through: :ingredients
+    validates :name, uniqueness: true
     serialize :nutrient_hash
     include ActiveModel::Serializers::JSON
     
